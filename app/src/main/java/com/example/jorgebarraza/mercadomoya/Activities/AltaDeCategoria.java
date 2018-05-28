0585f43d999dfadf34f34611e66e188ad665545d
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-05-27 20:52:49 -0600

    Añadida todas las pantallas
    -Se crearon las pantallas faltantes.
    -No hay codigo

---
+++ app/src/main/java/com/example/jorgebarraza/mercadomoya/Activities/AltaDeCategoria.java
@@ -0,0 +1,15 @@
+package com.example.jorgebarraza.mercadomoya.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jorgebarraza.mercadomoya.R;
+
+public class AltaDeCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_de_categoria);
+    }
+}
